27f0d3164d5328907af9fdc8ba9f5903284938d9
swapnilhande <swapnil.hande@gmail.com>
2018-12-18 18:49:43 -0500

    Adding input to output functionality

--- js/common.js
+++ js/common.js
@@ -2,18 +2,52 @@ $(document).ready(function () {
     $("#input-btn").click(function () {
         processInputPan();
         processInputToken();
-    })
+        processInputRegId();
+    });
+
+    $("#input-reset-btn").click(function () {
+        $("input[id^=input]").val('');
+    });
+
+    $("#output-reset-btn").click(function () {
+        $("input[id^=output]").val('');
+    });
 
 })
 
+function processInputRegId() {
+    let regId = $("#input-regid").val();
+    let first6 = regId.substr(0, 6);
+    let last4 = regId.substr(regId.length - 4, regId.length);
+    let mid = regId.substr(6, regId.length - 10);
+    let panMid = '999999999' - mid + '';
+    let tokenMid = panMid.split("").reverse().join("");
+    let pan = first6.split("").reverse().join("") + panMid + last4.split("").reverse().join("");
+    let token = first6.split("").reverse().join("") + tokenMid + last4.split("").reverse().join("");
+    $("#output-expdate").val("5001");
+    $("#output-pan").val(pan);
+    $("#output-token").val(token);
+    let secondToLastDigit = regId.substr(regId.length - 2, 1);
+    secondToLastDigit = secondToLastDigit % 4 + '';
+    let walletType = '', eci = '';
+    switch (secondToLastDigit) {
+        case "1": walletType = "ANDROID"; eci = "07"; break;
+        case "2": walletType = "APPLE"; eci = "05"; break;
+        case "3": walletType = "SAMSUNG"; eci = "07"; break;
+
+    }
+    $("#output-wallet").val(walletType);
+    $("#output-eci").val(eci);
+}
+
 function processInputPan () {
-    var pan = $("#input-pan").val();
-    var first6 = pan.substr(0, 6);
-    var last4 = pan.substr(pan.length - 4, pan.length);
-    var mid = pan.substr(6, pan.length - 10);
-    var regId = '999999999' - mid;
-    var output_mid = mid.split("").reverse().join("");
-    var isTokenNew = pan.substr(pan.length-4, 3);
+    let pan = $("#input-pan").val();
+    let first6 = pan.substr(0, 6);
+    let last4 = pan.substr(pan.length - 4, pan.length);
+    let mid = pan.substr(6, pan.length - 10);
+    let regId = '999999999' - mid;
+    let output_mid = mid.split("").reverse().join("");
+    let isTokenNew = pan.substr(pan.length-4, 3);
     $("#output-token").val(first6 + output_mid + last4);
     if (isTokenNew == '000') {
         $("#output-tokennewlygen").val("true");
@@ -25,12 +59,12 @@ function processInputPan () {
 }
 
 function processInputToken () {
-    var token = $("#input-token").val();
-    var first6 = token.substr(0, 6);
-    var last4 = token.substr(token.length - 4, token.length);
-    var mid = token.substr(6, token.length - 10);
+    let token = $("#input-token").val();
+    let first6 = token.substr(0, 6);
+    let last4 = token.substr(token.length - 4, token.length);
+    let mid = token.substr(6, token.length - 10);
     mid = mid.split("").reverse().join("");
-    var cvv = token.substr(token.length-4, 3);
+    let cvv = token.substr(token.length-4, 3);
     $("#output-pan").val(first6 + mid + last4);
     $("#output-cvv").val(cvv);
     $("#output-expdate").val("5001");
